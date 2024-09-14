0b937b76b60529e760f1798f9967ed9ce725e3df
Matheus <matheus.davo@yahoo.com.br>
2024-09-13 21:04:11 -0300

    final

--- meuPrograma.java
+++ meuPrograma.java
@@ -1,28 +1,34 @@
 import java.util.Scanner;
-public class meuPrograma{ 
-   public static void main(String args[]){ 
-   Scanner _scTrx = new Scanner(System.in);
-    int a;
-    double b;
-	String c;
-    double w;
-    int i;
-    int j;
-    double k;
-a = 0;
-b = 1.5;
-c = "ola";
-i = 0;
-j = 0;
-k = 200*(70/5.5);
-w = k+b;
-System.out.println(c);
-c = _scTrx.nextLine();
-if ((!(a==3))) {System.out.println("estou no if");
-}else {System.out.println("nao entrei");
-}while (i<9&&a<0) {i = i+1;
-System.out.println("estou no while");
-}do {j = j+1;
-System.out.println("estou no doWhile");
-}while (j<9);   }
+
+public class meuPrograma {
+    public static void main(String args[]) {
+        Scanner _scTrx = new Scanner(System.in);
+        int a;
+        double b;
+        String c;
+        int i;
+        int j;
+        double k;
+        a = 0;
+        b = 1.5;
+        c = "ola";
+        i = 0;
+        j = 0;
+        k = 200 * (70 / 5.5);
+        System.out.println(c);
+        c = _scTrx.nextLine();
+        if ((!(a == 3))) {
+            System.out.println("estou no if");
+        } else {
+            System.out.println("nao entrei");
+        }
+        while (i < 9 && a < 0) {
+            i = i + 1;
+            System.out.println("estou no while");
+        }
+        do {
+            j = j + 1;
+            System.out.println("estou no doWhile");
+        } while (j < 9);
+    }
 }
